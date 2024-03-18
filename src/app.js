2e3b17b5d3e693fa6eb72aeb1d2dedeef2ee262f
Carlos Daniel Hurtado <108777036+carlosdaniel236@users.noreply.github.com>
2024-03-18 05:17:02 +0000

    no se como correrlo pero creo que mi logica esta bien

--- src/app.js
+++ src/app.js
@@ -1,11 +1,61 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// Arrays con pronombres, adjetivos y sustantivos
+window.onload = () => {
+  const pronombres = ["mi", "tu", "su", "nuestro", "vuestro", "su"];
+  const adjetivos = [
+    "grandioso",
+    "fabuloso",
+    "increíble",
+    "genial",
+    "fantástico",
+    "innovador"
+  ];
+  const sustantivos = [
+    "sitio",
+    "blog",
+    "tienda",
+    "proyecto",
+    "academia",
+    "curso"
+  ];
+  const extensiones = [".com", ".info", ".gov", ".aprendo"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  // Función para generar un dominio aleatorio
+  function generarDominio() {
+    // Seleccionar aleatoriamente un pronombre, adjetivo y sustantivo
+    const pronombreAleatorio =
+      pronombres[Math.floor(Math.random() * pronombres.length)];
+    const adjetivoAleatorio =
+      adjetivos[Math.floor(Math.random() * adjetivos.length)];
+    const sustantivoAleatorio =
+      sustantivos[Math.floor(Math.random() * sustantivos.length)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    // Seleccionar una extensión aleatoria
+    const extensionAleatoria =
+      extensiones[Math.floor(Math.random() * extensiones.length)];
+
+    // Concatenar el dominio aleatorio
+    const dominioAleatorio =
+      pronombreAleatorio +
+      "-" +
+      adjetivoAleatorio +
+      "-" +
+      sustantivoAleatorio +
+      extensionAleatoria;
+
+    return dominioAleatorio;
+  }
+
+  // Generar 10 dominios aleatorios
+  for (let i = 0; i < 10; i++) {
+    generarDominioButton.addEventListener("click"), () => {};
+  }
 };
+
+generarExcusaButton.addEventListener("click", () => {
+  //creando el indice aleatorio
+  const indiceAleatorio = Math.floor(Math.random() * sujetos.length);
+  //generando y mostrando la excusa
+  const excusaGenerada = generarExcusa(indiceAleatorio);
+  //creando el click para el boton
+  excusaElement.textContent = excusaGenerada;
+});
