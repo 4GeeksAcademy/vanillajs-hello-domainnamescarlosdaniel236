c49f4c807555b39c6069becd360718fc589a7f33
Carlos Daniel Hurtado <108777036+carlosdaniel236@users.noreply.github.com>
2024-03-20 20:12:33 +0000

    llamdno la funcion para corregir

--- src/app.js
+++ src/app.js
@@ -1,49 +1,45 @@
 /* eslint-disable no-console */
 window.onload = () => {
   // Arrays con pronombres, adjetivos y sustantivos
-    let pronombres = [
-    'mi',
-    'tu',
-    'su',
-    'nuestro',
-    'vuestro',
-    'su'
-    ];
-    let adjetivos = [
-      'grandioso',
-      'fabuloso',
-      'increíble',
-      'genial',
-      'fantástico',
-      'innovador'
-    ];
-    let sustantivos = [
-      'sitio',
-      'blog',
-      'tienda',
-      'proyecto',
-      'academia',
-      'curso'
-    ];
-    let extensiones = [
-      '.com',
-      '.info',
-      '.gob',
-      '.aprendo'
-    ];
-    // Función para generar un dominio aleatorio
-function generarDominio() {
-  // Variable para almacenar el dominio generado
-  let dominioAleatorio = "";
-  // Bucle anidado para combinar pronombres, adjetivos, sustantivos y extensiones
-  for (let i = 0; i < pronombres.length; i++) {
-    for (let j = 0; j < adjetivos.length; j++) {
-      for (let k = 0; k < sustantivos.length; k++) {
-        for (let l = 0; l < extensiones.length; l++) {
-          dominioAleatorio = pronombres[i] + "-" + adjetivos[j] + "-" + sustantivos[k] + extensiones[l];
-          console.log('Dominio generado:', dominioAleatorio);}
+  let pronombres = ["mi", "tu", "su", "nuestro", "vuestro", "su"];
+  let adjetivos = [
+    "grandioso",
+    "fabuloso",
+    "increíble",
+    "genial",
+    "fantástico",
+    "innovador"
+  ];
+  let sustantivos = [
+    "sitio",
+    "blog",
+    "tienda",
+    "proyecto",
+    "academia",
+    "curso"
+  ];
+  let extensiones = [".com", ".info", ".gob", ".aprendo"];
+  // Función para generar un dominio aleatorio
+  function generarDominio() {
+    // Variable para almacenar el dominio generado
+    let dominioAleatorio = "";
+    // Bucle anidado para combinar pronombres, adjetivos, sustantivos y extensiones
+    for (let i = 0; i < pronombres.length; i++) {
+      for (let j = 0; j < adjetivos.length; j++) {
+        for (let k = 0; k < sustantivos.length; k++) {
+          for (let l = 0; l < extensiones.length; l++) {
+            dominioAleatorio =
+              pronombres[i] +
+              "-" +
+              adjetivos[j] +
+              "-" +
+              sustantivos[k] +
+              extensiones[l];
+            console.log("Dominio generado:", dominioAleatorio);
+          }
         }
       }
     }
   }
+  generarDominio();
 };
